c0c65d0b4e2dc9e1b90d6577aea2e1e072986bc9
minyu <1370302950@qq.com>
2023-09-20 19:27:17 +0800

    Added model

--- train.py
+++ train.py
@@ -1,1 +1,111 @@
-pass
+import numpy as np
+import os
+import pathlib
+# import PIL
+# import PIL.Image
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import imghdr
+from tensorflow import keras
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+image_path = "./dataset"
+data_dir = pathlib.Path(image_path)
+image_count = len(list(data_dir.glob('*/*.jpg')))
+
+image_extensions = [".jpg"]  # all my images file extensions
+
+img_type_accepted_by_tf = ["bmp", "gif", "jpeg", "png"]
+for filepath in pathlib.Path(data_dir).rglob("*"):
+    if filepath.suffix.lower() in image_extensions:
+        img_type = imghdr.what(filepath)
+        if img_type is None:
+            print(f"{filepath} is not an image")
+        elif img_type not in img_type_accepted_by_tf:
+            print(f"{filepath} is a {img_type}, not accepted by TensorFlow")
+
+# Check the total number of pics
+# print(image_count)
+
+# Check some pics
+# sample_pic = list(data_dir.glob('glass/*'))
+# PIL.Image.open(str(sample_pic[0]))
+
+# Now we use keras load pics
+batch_size = 32
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(class_names)
+
+# normalization_layer = tf.keras.layers.Rescaling(1. / 255)
+#
+# normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+# image_batch, labels_batch = next(iter(normalized_ds))
+# first_image = image_batch[0]
+# # Notice the pixel values are now in `[0,1]`.
+# print(np.min(first_image), np.max(first_image))
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+num_classes = 6
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Rescaling(1. / 255),
+    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(num_classes)
+])
+
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+
+#
+checkpoint_path = "./model/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+# Train the model with the new callback
+model.fit(train_ds,
+          epochs=20,
+          validation_data=val_ds,
+          callbacks=[cp_callback])  # Pass callback to training
+
+# This may generate warnings related to saving the state of the optimizer.
+# These warnings (and similar warnings throughout this notebook)
+# are in place to discourage outdated usage, and can be ignored.
+
+model.summary()
